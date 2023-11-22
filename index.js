38a20f996239aac33a592a705f907bc5d64b59ca
AlekseyZaqvozkin <126376056+AlekseyZaqvozkin@users.noreply.github.com>
2023-11-22 21:27:47 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+function auto_grow(el) {
+    // if (el.scrollHeight > el.clientHeight) 
+    	// el.style.height = el.scrollHeight + 'px'
+        
+            el.style.height = "5px";
+            el.style.height = (el.scrollHeight) + "px";
+         
+}
